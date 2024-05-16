4b6c4a611f48eb2a0d19dd7276e167771b32d9bb
anidiifeanyi <anidiifeanyi@gmail.com>
2024-05-16 10:46:38 +0100

    Configure SMTP

--- email-sender.py
+++ email-sender.py
@@ -20,3 +20,10 @@ em['From'] = email_sender
 em['To'] = email_receiver
 em[' subject'] = subject
 em.set_content(body)
+
+# Configure SMTP Sender
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL( 'smtp.gmail.com' , 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
