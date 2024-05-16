5f99b0a94ec95141d0b4256ec098943b05e746ab
anidiifeanyi <anidiifeanyi@gmail.com>
2024-05-16 10:43:10 +0100

    added email subject and body

--- email-sender.py
+++ email-sender.py
@@ -3,3 +3,13 @@ from dotenv import password
 import ssl
 import smtplib
 
+# Email credentials
+email_sender = 'Your email'
+email_password = password
+email_receiver = ''
+
+# email body
+subject = "Dont forget to subscribe"
+body = """
+When you watch a video, please hit subscribe
+"""
